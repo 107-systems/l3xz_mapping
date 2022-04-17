398fc890d68b3e7701f61595b261e0a95e059233
jw01979 <jonas.wuehr@stud.th-deg.de>
2022-04-17 16:38:59 +0200

    host app

--- host/host_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/l3xz/host_ws/src
-# Build directory: /home/l3xz/host_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("l3xz_mapping")
