3b85824838ba392adb6b0aa33b884682f06031d6
jw01979 <jonas.wuehr@stud.th-deg.de>
2022-05-01 21:40:05 +0200

    led checkboxes

--- host/host_ws/src/webgui/javascript/frontend_elrob.js
+++ host/host_ws/src/webgui/javascript/frontend_elrob.js
@@ -44,3 +44,51 @@ function button_download_onclick()
     vis4.download();
     vis5.download();
 }
+
+function set_rgb(topic, r_on, g_on, b_on)
+{
+  var client = new ROSLIB.Service({
+    ros : ros,
+    name : topic,
+    serviceType : 'l3xz_openmv_camera/rgb'
+  });
+
+  var request = new ROSLIB.ServiceRequest({
+    r : r_on,
+    g : g_on,
+    b : b_on
+  });
+
+  client.callService(request, function(result) { console.log(result);});
+}
+
+function set_ir(topic, ir_on)
+{
+  var client = new ROSLIB.Service({
+    ros : ros,
+    name : topic,
+    serviceType : 'l3xz_openmv_camera/ir'
+  });
+
+  var request = new ROSLIB.ServiceRequest({
+    on : ir_on
+  });
+
+  client.callService(request, function(result) { console.log(result);});
+}
+
+function thermal_lighting_onclick()
+{
+  set_rgb('/l3xz/openmv_thermal/rgb', document.getElementById("thermal_red").checked,
+          document.getElementById("thermal_green").checked,
+          document.getElementById("thermal_blue").checked);
+  set_ir('/l3xz/openmv_thermal/ir', document.getElementById("thermal_ir").checked);
+}
+
+function rgb_lighting_onclick()
+{
+  set_rgb('/l3xz/openmv_rgb/rgb', document.getElementById("rgb_red").checked,
+          document.getElementById("rgb_green").checked,
+          document.getElementById("rgb_blue").checked);
+  set_ir('/l3xz/openmv_rgb/ir', document.getElementById("rgb_ir").checked);
+}
